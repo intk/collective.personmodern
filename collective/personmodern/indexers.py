0871de49262fd3a50499d8ff7b1557ef0b27b5c0
andreesg <andreslb1@gmail.com>
2019-07-23 15:43:11 +0200

    Add correct translations for person view fields

--- collective/personmodern/indexers.py
+++ collective/personmodern/indexers.py
@@ -1,14 +1,30 @@
 # -*- coding: utf-8 -*-
 from zope.component import getUtility
 from plone.indexer.decorator import indexer
-
 from person import IPerson
 
 from plone.i18n.normalizer.interfaces import INormalizer
+from plone.app.contenttypes.behaviors.collection import ICollection
 
 @indexer(IPerson)
-def lastname(person):
-    return getUtility(INormalizer).normalize(person.lastname)
+def person_priref(person):
+    try:
+        terms = []
+        normalized_id = getUtility(INormalizer).normalize(person.priref)
+        if normalized_id:
+            terms.append(normalized_id)
+        return terms
+    except:
+        return []
 
+@indexer(IPerson)
+def objects_count(person):
+    try:
+        brains = ICollection(person).results(batch=False)
+        total = len(brains)
+        total_value = "%s" %(str(total))
+        return total_value
+    except:
+        return ""
     
     
--- collective/personmodern/person.py
+++ collective/personmodern/person.py
@@ -3,8 +3,12 @@ from zope.interface import Interface, implements
 from plone.dexterity.content import Item, Container
 from plone.app.content.interfaces import INameFromTitle
 from plone.dexterity.utils import safe_unicode
-
+from zope.interface import provider
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.supermodel import model
 from plone.app.content.interfaces import INameFromTitle
+from collective.personmodern import MessageFactory as _
+from zope import schema
 
 class INameFromPersonNames(INameFromTitle):
     def title():
@@ -17,27 +21,12 @@ class NameFromPersonNames(object):
         self.context = context
 
     def generate_title(self):
-        names_list = [getattr(self.context, 'firstname', None), getattr(self.context, 'middlename', None), getattr(self.context, 'lastname', None)]
-        names = " ".join([name for name in names_list if name])
-
-        names = ""
-
-        extra = []
-        if hasattr(self.context, 'nationality'):
-            if self.context.nationality:
-                extra.append(self.context.nationality)
-        if hasattr(self.context, 'year'):
-            if self.context.year:
-                extra.append(self.context.year)
-
-        extra_text = ', '.join(extra)
-
-        if names and extra_text:
-            final_text = "%s (%s)" %(names, extra_text)
-            return final_text
-        elif names and not extra_text:
-            final_text = "%s" %(names)
-            return final_text
+        name = getattr(self.context, 'name', None)
+        if name:
+            # this is a CMF accessor, so should return utf8-encoded
+            if isinstance(name, unicode):
+                return name.encode('utf-8')
+            return name
         else:
             # this is a CMF accessor, so should return utf8-encoded
             if isinstance(self.context.title, unicode):
@@ -60,26 +49,12 @@ class Person(Container):
     implements(IPerson)
 
     def generate_title(self):
-        names = ""
-        names_list = [getattr(self, 'firstname', None), getattr(self, 'middlename', None), getattr(self, 'lastname', None)]
-        names = " ".join([name for name in names_list if name])
-
-        extra = []
-        if hasattr(self, 'nationality'):
-            if self.nationality:
-                extra.append(self.nationality)
-        if hasattr(self, 'year'):
-            if self.year:
-                extra.append(self.year)
-
-        extra_text = ', '.join(extra)
-
-        if names and extra_text:
-            final_text = "%s (%s)" %(names, extra_text)
-            return final_text
-        elif names and not extra_text:
-            final_text = "%s" %(names)
-            return final_text
+        name = getattr(self, 'name', None)
+        if name:
+            # this is a CMF accessor, so should return utf8-encoded
+            if isinstance(name, unicode):
+                return name.encode('utf-8')
+            return name
         else:
             # this is a CMF accessor, so should return utf8-encoded
             if isinstance(self.title, unicode):
@@ -96,6 +71,55 @@ class Person(Container):
     def setTitle(self, title):
         # Set Dublin Core Title element - resource name.
         self.title = safe_unicode(title)
+
+
+
+@provider(IFormFieldProvider)
+class ICreator(model.Schema):
+    """Interface for Exhibition behavior."""
+
+    # exhibition fieldset
+    model.fieldset(
+        'creator',
+        label=_(u'Creator', default=u'Creator'),
+        fields=['place_of_birth', 'place_of_death', "birth_date_start", "birth_date_end", "death_date_start", "death_date_end", "persistent_url"],
+    )
+
+    place_of_birth = schema.TextLine(
+        title=_(u'place_of_birth', default=u'Place of birth'),
+        required=False
+    )
+
+    place_of_death = schema.TextLine(
+        title=_(u'place_of_death', default=u'Place of death'),
+        required=False
+    )
+
+    birth_date_start = schema.TextLine(
+        title=_(u'birth_date_start', default=u'Birth data start'),
+        required=False
+    )
+
+    birth_date_end = schema.TextLine(
+        title=_(u'birth_date_end', default=u'Birth date end'),
+        required=False
+    )
+
+    death_date_start = schema.TextLine(
+        title=_(u'death_date_start', default=u'Death date start'),
+        required=False
+    )
+
+    death_date_end = schema.TextLine(
+        title=_(u'death_date_end', default=u'Death date end'),
+        required=False
+    )
+
+    persistent_url = schema.TextLine(
+        title=_(u'persistent_url', default=u'Persistent url'),
+        required=False
+    )
+
 
 
     
